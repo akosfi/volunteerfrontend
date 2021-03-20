940ce2f65ca4f1995eb405cc36f3d06831bf1d46
Akos Fi <fi.akos@straxus.hu>
2021-03-20 18:15:02 +0100

    [feature] added material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { FC } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 //
 import "assets/styles/global.scss";
 
-function App() {
+const App: FC = () => {
+    const theme = createMuiTheme({
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 900,
+                md: 1024,
+                lg: 1280,
+                xl: 1920
+            }
+        }
+    });
+
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact>
-                    <p>Example</p>
-                </Route>
-            </Switch>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Switch>
+                    <Route path="/" exact>
+                        <p>Example</p>
+                    </Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
     );
-}
+};
 
 export default App;
