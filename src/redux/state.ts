6ae8f1471f2b761fd0e44033b74526d813f38324
Akos Fi <fi.akos@straxus.hu>
2021-03-20 22:53:36 +0100

    [fix] Minor config + state fix.

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,7 +1,11 @@
+import { EventState, initialState as initialEventState } from "./events/reducer";
+
 export type StoreState = {
+    event: EventState;
 };
 
 const initialState: StoreState = {
+    event: initialEventState
 };
 
 export default initialState;
